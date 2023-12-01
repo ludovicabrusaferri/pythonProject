298d5e9aa57b54195ed2d32603898fffc2e85704
Ludovica Brusaferri <l.brusaferri.16@ucl.ac.uk>
2023-12-01 17:16:55 +0000

    init

--- correlation.py
+++ correlation.py
@@ -37,34 +37,7 @@ def compute_correlation(trueplasma_directory, signaldata_directory):
             # Append Vt values for the current ROI
             all_trueplasma_vt_values.append(trueplasma_vt_all)
             all_signaldata_vt_values.append(signaldata_vt_all)
-
-            # Average over subjects (columns)
-    trueplasma_vt_mean = np.mean(all_trueplasma_vt_values, axis=1)
-    signaldata_vt_mean = np.mean(all_signaldata_vt_values, axis=1)
-
-    # Compute correlation for the mean values
-    correlation, _ = pearsonr(trueplasma_vt_mean, signaldata_vt_mean)
-    correlations.append(correlation)
-
-    # Plotting with regression line
-    plt.figure()
-    plt.plot(trueplasma_vt_mean, signaldata_vt_mean, 'o', label='Data Points')
-
-    # Fit a linear regression model
-    model = LinearRegression()
-    trueplasma_vt_mean = trueplasma_vt_mean.reshape(-1, 1)
-    model.fit(trueplasma_vt_mean, signaldata_vt_mean)
-
-    # Plot the regression line
-    plt.plot(trueplasma_vt_mean, model.predict(trueplasma_vt_mean), color='red', linewidth=2,
-             label='Regression Line')
-
-    plt.title(f'Correlation for Subject {subject_id} and ROI {roi}: {correlation:.2f}')
-    plt.xlabel('TRUEPLASMA Vt')
-    plt.ylabel('SIGNALDATA Vt')
-    plt.legend()
-    plt.show()
-
+    print(all_trueplasma_vt_values)
 
 
 # Specify the directories for TRUEPLASMA and SIGNALDATA outputs
